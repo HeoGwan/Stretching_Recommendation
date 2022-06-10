9c4f892e5e6ab7abf650bfc5c4bef009e3f32200
wjdgns5 <rhfovkeh5@naver.com>
2022-06-10 21:05:34 +0900

    체형 선택 설명 화면 추가

--- app/src/main/java/com/example/class_project/SelectActivity.java
+++ app/src/main/java/com/example/class_project/SelectActivity.java
@@ -98,6 +98,7 @@ public class SelectActivity extends AppCompatActivity {
     Form upperBody, lowerBody;
 
     ImageView showForm;
+    ImageView[] infos = new ImageView[3];
 
     Button btnOK;
 
@@ -116,6 +117,10 @@ public class SelectActivity extends AppCompatActivity {
     Integer[] RdoBtnInfoIDs = {R.id.normalNeckInfo, R.id.straightNeckInfo, R.id.forwardNeckInfo,
             R.id.flatShoulderInfo, R.id.normalShoulderInfo, R.id.slopeShoulderInfo,
             R.id.normalPelvicTiltInfo, R.id.anteriorPelvicTiltInfo, R.id.posteriorPelvicTiltInfo};
+
+    Integer[] infoIDs = {R.id.neckInfo, R.id.shoulderInfo, R.id.backInfo};
+
+    Class[] infoActivity = {popupactivity.class, popupactivity1.class, popupactivity2.class};
 
     SharedPreferences pref;
     SharedPreferences.Editor editor;
@@ -299,6 +304,19 @@ public class SelectActivity extends AppCompatActivity {
         selectNeck = (RadioGroup) findViewById(R.id.selectNeck);
         selectShoulder = (RadioGroup) findViewById(R.id.selectShoulder);
         selectBack = (RadioGroup) findViewById(R.id.selectBack);
+
+        for(int i = 0; i < infoIDs.length; ++i) {
+            int index = i;
+            infos[index] = (ImageView) findViewById(infoIDs[index]);
+
+            infos[index].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent popup = new Intent(getApplicationContext(), infoActivity[index]);
+                    startActivity(popup);
+                }
+            });
+        }
 
         // 목, 어깨, 허리정보 초기화
         for (int i = 0; i < RdoBtnIDs.length; ++i) {
---
+++ app/src/main/java/com/example/class_project/popupactivity2.java
@@ -0,0 +1,25 @@
+package com.example.class_project;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+
+public class popupactivity2 extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.popupactivity2);
+
+        Button closePopup = (Button) findViewById(R.id.closePopup);
+
+        closePopup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
